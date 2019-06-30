db448c2d9c4a223243dfb2483c56a2e155e07976
Daryk Kohler <daryk.kohler@gmail.com>
2019-06-29 17:19:58 -0700

    Setup reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+    return [
+        { title: 'Forever Flame', duration: '4:14' },
+        { title: 'Boom', duration: '2:17' },
+        { title: 'Midnight Grow', duration: '3:24' },
+        { title: '20/20', duration: '3:19' }
+    ]
+}
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload
+    }
+
+    return selectedSong
+}
