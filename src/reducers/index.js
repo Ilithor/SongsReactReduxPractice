67414fc3312b5ea520d2d9d0a001c3f0fd710b82
Daryk Kohler <daryk.kohler@gmail.com>
2019-06-29 17:27:46 -0700

    Linked reducers to index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux'
+
 const songsReducer = () => {
     return [
         { title: 'Forever Flame', duration: '4:14' },
@@ -14,3 +16,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
     return selectedSong
 }
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+})
